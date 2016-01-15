152749f4937dd92a5b9c7436ac779d2e5b28ff79
gooru-nishi <nishi@gooru.org>
2016-01-15 22:55:10 +0530

    code review inputs worked upon

--- src/main/java/org/gooru/nucleus/handlers/resources/processors/repositories/activejdbc/dbhandlers/DBHelper.java
+++ src/main/java/org/gooru/nucleus/handlers/resources/processors/repositories/activejdbc/dbhandlers/DBHelper.java
@@ -2,8 +2,8 @@ package org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import org.gooru.nucleus.handlers.resources.processors.repositories.ResourceRepo;
 import org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.entities.AJEntityResource;
+import org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.entities.ResourceEntityConstants;
 import org.javalite.activejdbc.LazyList;
 import org.postgresql.util.PGobject;
 import org.slf4j.Logger;
@@ -17,85 +17,95 @@ import java.util.Map;
 
 public class DBHelper {
   private static final Logger LOGGER = LoggerFactory.getLogger(DBHelper.class);
+  
+  static protected int NUM_RETRIES = 2;
 
-  private DBHelper() {
-    // TODO Auto-generated constructor stub
-  }
-
-  public static DBHelper getInstance() {
-    return Holder.INSTANCE;
-  }
-
-  public AJEntityResource getResourceById(String resourceId) {
-    String sql = "SELECT " + String.join(", ", ResourceRepo.attributes) + " FROM CONTENT WHERE id = '" + resourceId + "' AND content_format ='"
-      + ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE + "'";
-
-    LazyList<AJEntityResource> result = AJEntityResource.findBySQL(sql);
-    LOGGER.debug("getResourceById : {} ", result.toString());
-
-    if (result.size() > 0) {
-      LOGGER.debug("getResourceById : Return Value : {} ", result.toString());
-      return result.get(0);
-      //return new AJResponseJsonTransformer().transform(result.get(0).toJson(false, ResourceRepo.attributes));
+  static protected AJEntityResource getResourceById(String resourceId) {
+    try {
+      String sql = " SELECT " + String.join(", ", ResourceEntityConstants.attributes) + 
+                   " FROM content WHERE id = ?  AND content_format = ? AND is_deleted = false";
+  
+      PGobject contentFormat = new PGobject();
+      contentFormat.setType(ResourceEntityConstants.CONTENT_FORMAT_TYPE);
+      contentFormat.setValue(ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE);
+      
+      LazyList<AJEntityResource> result = AJEntityResource.findBySQL(sql, resourceId, contentFormat);
+      LOGGER.debug("getResourceById : {} ", result.toString());
+  
+      if (result.size() > 0) {
+        LOGGER.debug("getResourceById : Return Value : {} ", result.toString());
+        return result.get(0);
+      }
+  
+      LOGGER.warn("getResourceById : Resource with id : {} : not found", resourceId);
+    } catch (SQLException se) {
+      LOGGER.error("getResourceById : SQL Exception caught ! : {} ", se);
     }
-
-    LOGGER.warn("getResourceById : Resource with id : {} : not found", resourceId);
-
     return null;
   }
 
   /*
    * getDuplicateResourcesByURL: returns NULL if no duplicates
    */
-  protected JsonObject getDuplicateResourcesByURL(String inputURL) {
-    String sql = "SELECT id FROM content WHERE url = '" + inputURL + "' AND content_format = '" + ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE
-      + "' AND original_content_id is null";
-
-    LazyList<AJEntityResource> result = AJEntityResource.findBySQL(sql);
-    LOGGER.debug("getDuplicateResourcesByURL ! : {} ", result.toString());
-
+  static protected JsonObject getDuplicateResourcesByURL(String inputURL) {
     JsonObject returnValue = null;
-    if (result.size() > 0) {
-      returnValue = new JsonObject().put("duplicate_ids", new JsonArray(result.collect("id")));
+    
+    try {
+      String sql = "SELECT id FROM content"
+                 + " WHERE url = ? AND content_format = ? AND original_content_id is null AND is_deleted = false";
+  
+      PGobject contentFormat = new PGobject();
+      contentFormat.setType(ResourceEntityConstants.CONTENT_FORMAT_TYPE);
+      contentFormat.setValue(ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE);
+        
+      LazyList<AJEntityResource> result = AJEntityResource.findBySQL(sql, inputURL, contentFormat);
+      LOGGER.debug("getDuplicateResourcesByURL ! : {} ", result.toString());
+  
+      if (result.size() > 0) {
+        returnValue = new JsonObject().put("duplicate_ids", new JsonArray(result.collect(ResourceEntityConstants.RESOURCE_ID)));
+      }
+      return returnValue;
+    } catch (SQLException se) {
+      LOGGER.error("getDuplicateResourcesByURL ! : {} ", se);
+      return returnValue;
     }
-    return returnValue;
   }
 
   /*
    * populateEntityFromJson : throws exceptions
    */
-  protected void populateEntityFromJson(JsonObject inputJson, AJEntityResource resource) throws SQLException, IllegalArgumentException {
+  static protected void populateEntityFromJson(JsonObject inputJson, AJEntityResource resource) throws SQLException, IllegalArgumentException {
     String mapValue;
     for (Map.Entry<String, Object> entry : inputJson) {
       mapValue = (entry.getValue() != null) ? entry.getValue().toString() : null;
 
-      if (Arrays.asList(ResourceRepo.NOTNULL_FIELDS).contains(entry.getKey())) {
+      if (Arrays.asList(ResourceEntityConstants.NOTNULL_FIELDS).contains(entry.getKey())) {
         if (mapValue == null || mapValue.isEmpty()) {
           throw new IllegalArgumentException("Null value input for : " + entry.getKey());
         }
       }
 
-      if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_FORMAT)) {
-        if (!mapValue.equalsIgnoreCase(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
+      if (ResourceEntityConstants.CONTENT_FORMAT.equalsIgnoreCase(entry.getKey())) {
+        if (!ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE.equalsIgnoreCase(mapValue)) {
           throw new IllegalArgumentException("content format should always be a 'resource' but {} has been sent: " + mapValue);
         } else {
           PGobject contentFormat = new PGobject();
-          contentFormat.setType(ResourceRepo.CONTENT_FORMAT_TYPE);
+          contentFormat.setType(ResourceEntityConstants.CONTENT_FORMAT_TYPE);
           contentFormat.setValue(mapValue);
           resource.set(entry.getKey(), contentFormat);
         }
-      } else if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_SUBFORMAT)) {
-        if (!mapValue.contains(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
+      } else if (ResourceEntityConstants.CONTENT_SUBFORMAT.equalsIgnoreCase(entry.getKey())) {
+        if (mapValue == null || mapValue.isEmpty() || !mapValue.endsWith(ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
           throw new IllegalArgumentException("content sub format is not a valid resource format ; {} has been sent: " + mapValue);
         } else {
           PGobject contentSubformat = new PGobject();
-          contentSubformat.setType(ResourceRepo.CONTENT_SUBFORMAT_TYPE);
+          contentSubformat.setType(ResourceEntityConstants.CONTENT_SUBFORMAT_TYPE);
           contentSubformat.setValue(mapValue);
           resource.set(entry.getKey(), contentSubformat);
         }
-      } else if (Arrays.asList(ResourceRepo.JSONB_FIELDS).contains(entry.getKey())) {
+      } else if (Arrays.asList(ResourceEntityConstants.JSONB_FIELDS).contains(entry.getKey())) {
         PGobject jsonbFields = new PGobject();
-        jsonbFields.setType(ResourceRepo.JSONB_FORMAT);
+        jsonbFields.setType(ResourceEntityConstants.JSONB_FORMAT);
         jsonbFields.setValue(mapValue);
         resource.set(entry.getKey(), jsonbFields);
       } else {
@@ -112,7 +122,7 @@ public class DBHelper {
    * NOTE: This method does not do a lot of null checks etc; as all checks are
    * already done by UpdateResource() method.
    */
-  protected int updateOwnerDataToCopies(String ownerResourceId, JsonObject dataToBePropogated, String originalCreator)
+  static protected int updateOwnerDataToCopies(String ownerResourceId, JsonObject dataToBePropogated, String originalCreator)
     throws SQLException, IllegalArgumentException {
     LOGGER.debug("updateOwnerDataToCopies: OwnerResourceID {}", ownerResourceId);
     int numRecsUpdated = 0;
@@ -121,31 +131,38 @@ public class DBHelper {
     String updateStmt = null;
     for (Map.Entry<String, Object> entry : dataToBePropogated) {
       mapValue = (entry.getValue() != null) ? entry.getValue().toString() : null;
+      
+      if (Arrays.asList(ResourceEntityConstants.NOTNULL_FIELDS).contains(entry.getKey())) {
+        if (mapValue == null || mapValue.isEmpty()) {
+          throw new IllegalArgumentException("Null value input for : " + entry.getKey());
+        }
+      }
+
       LOGGER.debug("updateOwnerDataToCopies: OwnerResourceID {}", entry.getKey());
 
       updateStmt = (updateStmt == null) ? entry.getKey() + " = ?" : updateStmt + ", " + entry.getKey() + " = ?";
 
-      if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_FORMAT)) {
-        if (!mapValue.equalsIgnoreCase(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
+      if (ResourceEntityConstants.CONTENT_FORMAT.equalsIgnoreCase(entry.getKey())) {
+        if (!ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE.equalsIgnoreCase(mapValue)) {
           throw new IllegalArgumentException("content format should always be a 'resource' but {} has been sent: " + mapValue);
         } else {
           PGobject contentFormat = new PGobject();
-          contentFormat.setType(ResourceRepo.CONTENT_FORMAT_TYPE);
+          contentFormat.setType(ResourceEntityConstants.CONTENT_FORMAT_TYPE);
           contentFormat.setValue(entry.getValue().toString());
           params.add(contentFormat);
         }
-      } else if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_SUBFORMAT)) {
-        if (!mapValue.contains(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
+      } else if (ResourceEntityConstants.CONTENT_SUBFORMAT.equalsIgnoreCase(entry.getKey())) {
+        if (mapValue == null || mapValue.isEmpty() || !mapValue.endsWith(ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
           throw new IllegalArgumentException("content sub format is not a valid resource format ; {} has been sent: " + mapValue);
         } else {
           PGobject contentSubformat = new PGobject();
-          contentSubformat.setType(ResourceRepo.CONTENT_SUBFORMAT_TYPE);
+          contentSubformat.setType(ResourceEntityConstants.CONTENT_SUBFORMAT_TYPE);
           contentSubformat.setValue(entry.getValue().toString());
           params.add(contentSubformat);
         }
-      } else if (Arrays.asList(ResourceRepo.JSONB_FIELDS).contains(entry.getKey())) {
+      } else if (Arrays.asList(ResourceEntityConstants.JSONB_FIELDS).contains(entry.getKey())) {
         PGobject jsonbFields = new PGobject();
-        jsonbFields.setType(ResourceRepo.JSONB_FORMAT);
+        jsonbFields.setType(ResourceEntityConstants.JSONB_FORMAT);
         jsonbFields.setValue(entry.getValue().toString());
         params.add(jsonbFields);
       } else {
@@ -158,15 +175,11 @@ public class DBHelper {
     if (updateStmt != null) {
       params.add(ownerResourceId);
       params.add(originalCreator);
-      numRecsUpdated = AJEntityResource.update(updateStmt, "original_content_id = ? AND original_creator_id = ? ", params.toArray());
+      numRecsUpdated = AJEntityResource.update(updateStmt, "original_content_id = ? AND original_creator_id = ? AND is_deleted = false", params.toArray());
       LOGGER.debug("updateOwnerDataToCopies : Update successful. Number of records updated: {}", numRecsUpdated);
     }
 
     return numRecsUpdated;
   }
-
-  private static class Holder {
-    private static final DBHelper INSTANCE = new DBHelper();
-  }
-
+  
 }
--- src/main/java/org/gooru/nucleus/handlers/resources/processors/repositories/activejdbc/dbhandlers/UpdateResourceHandler.java
+++ src/main/java/org/gooru/nucleus/handlers/resources/processors/repositories/activejdbc/dbhandlers/UpdateResourceHandler.java
@@ -2,8 +2,8 @@ package org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.
 
 import io.vertx.core.json.JsonObject;
 import org.gooru.nucleus.handlers.resources.processors.ProcessorContext;
-import org.gooru.nucleus.handlers.resources.processors.repositories.ResourceRepo;
 import org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.entities.AJEntityResource;
+import org.gooru.nucleus.handlers.resources.processors.repositories.activejdbc.entities.ResourceEntityConstants;
 import org.gooru.nucleus.handlers.resources.processors.responses.ExecutionResult;
 import org.gooru.nucleus.handlers.resources.processors.responses.MessageResponse;
 import org.gooru.nucleus.handlers.resources.processors.responses.MessageResponseFactory;
@@ -31,11 +31,12 @@ class UpdateResourceHandler implements DBHandler {
   @Override
   public ExecutionResult<MessageResponse> checkSanity() {
     if (context.request() == null || context.request().isEmpty()) {
-      LOGGER.debug("checkSanity() failed with invalid json. ");
+      LOGGER.error("checkSanity() failed with invalid json. ");
       return new ExecutionResult<>(MessageResponseFactory.createInvalidRequestResponse(), ExecutionResult.ExecutionStatus.FAILED);
     }
+    
     if (context.resourceId() == null || context.resourceId().isEmpty()) {
-      LOGGER.debug("checkSanity() failed with invalid resourceid. ");
+      LOGGER.error("checkSanity() failed with invalid resourceid. ");
       return new ExecutionResult<>(MessageResponseFactory.createInvalidRequestResponse(), ExecutionResult.ExecutionStatus.FAILED);
     }
 
@@ -47,12 +48,12 @@ class UpdateResourceHandler implements DBHandler {
   public ExecutionResult<MessageResponse> validateRequest() {
     try {
       // fetch resource from DB based on Id received
-      AJEntityResource fetchDBResourceData = DBHelper.getInstance().getResourceById(context.resourceId());
+      AJEntityResource fetchDBResourceData = DBHelper.getResourceById(context.resourceId());
       if (fetchDBResourceData == null) {
         LOGGER.error("validateRequest : updateResource : Object to update is not found in DB! Input resource ID: {} ", context.resourceId());
         return new ExecutionResult<>(MessageResponseFactory.createNotFoundResponse(), ExecutionResult.ExecutionStatus.FAILED);
       }
-      String originalCreator = fetchDBResourceData.getString(ResourceRepo.ORIGINAL_CREATOR_ID);
+      String originalCreator = fetchDBResourceData.getString(ResourceEntityConstants.ORIGINAL_CREATOR_ID);
       LOGGER.debug("validateRequest : updateResource : Original creator from DB = {}.", originalCreator);
 
       if ((originalCreator != null) && !originalCreator.isEmpty()) {
@@ -67,7 +68,7 @@ class UpdateResourceHandler implements DBHandler {
       // compare input value and collect only changed attributes in new model
       // that we will use to update
       updateRes = new AJEntityResource();
-      updateRes.set(ResourceRepo.RESOURCE_ID, context.resourceId());
+      updateRes.set(ResourceEntityConstants.RESOURCE_ID, context.resourceId());
 
       LOGGER.debug("validateRequest updateResource : Iterate through the input Json now.");
 
@@ -75,7 +76,7 @@ class UpdateResourceHandler implements DBHandler {
         LOGGER.debug("validateRequest updateResource : checking the key & values..before collection. Key: {}", entry.getKey());
 
         mapValue = (entry.getValue() != null) ? entry.getValue().toString() : null;
-        if (Arrays.asList(ResourceRepo.NOTNULL_FIELDS).contains(entry.getKey())) {
+        if (Arrays.asList(ResourceEntityConstants.NOTNULL_FIELDS).contains(entry.getKey())) {
           if (mapValue == null) {
             LOGGER.error("validateRequest Failed to update resource. Field : {} : is mandatory field and cannot be null.", entry.getKey());
             return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
@@ -85,11 +86,11 @@ class UpdateResourceHandler implements DBHandler {
 
         // mandatory and owner specific items may be overlapping...so do a
         // separate check not as ELSE condition
-        if (!isOwner && Arrays.asList(ResourceRepo.OWNER_SPECIFIC_FIELDS).contains(entry.getKey())) {
+        if (!isOwner && Arrays.asList(ResourceEntityConstants.OWNER_SPECIFIC_FIELDS).contains(entry.getKey())) {
           // LOGGER.debug("validateRequest updateResource : Not owner but changing owner specific fields?");
           LOGGER.error("Error updating resource. Field: {} : can be updated only by owner of the resource.", entry.getKey());
           return new ExecutionResult<>(MessageResponseFactory.createForbiddenResponse(), ExecutionResult.ExecutionStatus.FAILED);
-        } else if (isOwner && Arrays.asList(ResourceRepo.OWNER_SPECIFIC_FIELDS).contains(entry.getKey())) {
+        } else if (isOwner && Arrays.asList(ResourceEntityConstants.OWNER_SPECIFIC_FIELDS).contains(entry.getKey())) {
           // collect the DB fields to update for owner specific fields across
           // all
           // copies of this resource
@@ -102,74 +103,44 @@ class UpdateResourceHandler implements DBHandler {
         }
 
         // collect the attributes and values in the model.
-        if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_FORMAT)) {
-          if (mapValue == null || mapValue.isEmpty()) {
-            LOGGER.error("updateResource : content format is null! : {} ", entry.getKey());
-            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-              ExecutionResult.ExecutionStatus.FAILED);
-          } else {
-            if (!mapValue.equalsIgnoreCase(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
-              return new ExecutionResult<>(
-                MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-                ExecutionResult.ExecutionStatus.FAILED);
-            } else {
-              PGobject contentFormat = new PGobject();
-              contentFormat.setType(ResourceRepo.CONTENT_FORMAT_TYPE);
-              contentFormat.setValue(mapValue);
-              updateRes.set(entry.getKey(), contentFormat);
-            }
+        if (ResourceEntityConstants.CONTENT_FORMAT.equalsIgnoreCase(entry.getKey())) {
+          if (!ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE.equalsIgnoreCase(mapValue)) {
+            LOGGER.error("updateResource : content format is invalid! : {} ", entry.getKey());
+            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())), ExecutionResult.ExecutionStatus.FAILED);
           }
-        } else if (entry.getKey().equalsIgnoreCase(ResourceRepo.CONTENT_SUBFORMAT)) {
-          if (mapValue == null || mapValue.isEmpty()) {
-            LOGGER.error("updateResource : content subformat is null! : {} ", entry.getKey());
-            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-              ExecutionResult.ExecutionStatus.FAILED);
+        } else if (ResourceEntityConstants.CONTENT_SUBFORMAT.equalsIgnoreCase(entry.getKey())) {
+          if (mapValue == null || mapValue.isEmpty() || !mapValue.endsWith(ResourceEntityConstants.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
+            LOGGER.error("updateResource : content subformat is invalid! : {} ", entry.getKey());
+            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())), ExecutionResult.ExecutionStatus.FAILED);
           } else {
-            if (!mapValue.contains(ResourceRepo.VALID_CONTENT_FORMAT_FOR_RESOURCE)) {
-              return new ExecutionResult<>(
-                MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-                ExecutionResult.ExecutionStatus.FAILED);
-            } else {
-              PGobject contentSubformat = new PGobject();
-              contentSubformat.setType(ResourceRepo.CONTENT_SUBFORMAT_TYPE);
-              contentSubformat.setValue(mapValue);
-              updateRes.set(entry.getKey(), contentSubformat);
-            }
+            PGobject contentSubformat = new PGobject();
+            contentSubformat.setType(ResourceEntityConstants.CONTENT_SUBFORMAT_TYPE);
+            contentSubformat.setValue(mapValue);
+            updateRes.set(entry.getKey(), contentSubformat);
           }
-        } else if (Arrays.asList(ResourceRepo.JSONB_FIELDS).contains(entry.getKey())) {
-          if (Arrays.asList(ResourceRepo.NOTNULL_FIELDS).contains(entry.getKey())) {
+        } else if (Arrays.asList(ResourceEntityConstants.JSONB_FIELDS).contains(entry.getKey())) {
+          if (Arrays.asList(ResourceEntityConstants.NOTNULL_FIELDS).contains(entry.getKey())) {
             if (mapValue == null || mapValue.isEmpty()) {
               LOGGER.error("updateResource : mandatory fields is null! : {} ", entry.getKey());
-              return new ExecutionResult<>(
-                MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-                ExecutionResult.ExecutionStatus.FAILED);
+              return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())), ExecutionResult.ExecutionStatus.FAILED);
             } else {
               PGobject jsonbFields = new PGobject();
-              jsonbFields.setType(ResourceRepo.JSONB_FORMAT);
+              jsonbFields.setType(ResourceEntityConstants.JSONB_FORMAT);
               jsonbFields.setValue(mapValue);
               updateRes.set(entry.getKey(), jsonbFields);
             }
           }
-
         } else {
           if (mapValue == null || mapValue.isEmpty()) {
             LOGGER.error("updateResource : mandatory fields in else is null! : {} ", entry.getKey());
-            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())),
-              ExecutionResult.ExecutionStatus.FAILED);
+            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(new JsonObject().put(entry.getKey(), entry.getValue())), ExecutionResult.ExecutionStatus.FAILED);
           } else {
-            updateRes.set(entry.getKey(), entry.getValue()); // intentionally
-            // kept
-            // entry.getValue
-            // instead of
-            // mapValue as it
-            // needs to
-            // handle other
-            // datatypes like
-            // boolean
+            updateRes.set(entry.getKey(), entry.getValue()); // intentionally kept entry.getValue instead of mapValue as it needs to handle other datatypes like boolean
           }
         }
       }
     } catch (SQLException e) {
+      LOGGER.error("updateResource : SQLException caught! {} ", e);
       return new ExecutionResult<>(MessageResponseFactory.createInternalErrorResponse(e.getMessage()), ExecutionResult.ExecutionStatus.FAILED);
     }
     return new ExecutionResult<>(null, ExecutionResult.ExecutionStatus.CONTINUE_PROCESSING);
@@ -177,26 +148,31 @@ class UpdateResourceHandler implements DBHandler {
 
   @Override
   public ExecutionResult<MessageResponse> executeRequest() {
-    if (updateRes != null) {
-      if (!updateRes.save()) {
-        LOGGER.debug("executeRequest : Update resource failed! ");
-        return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(updateRes.errors()),
-          ExecutionResult.ExecutionStatus.FAILED);
-      } else {
-        if (ownerDataToPropogateToCopies != null) {
-          try {
-            DBHelper.getInstance().updateOwnerDataToCopies(context.resourceId(), ownerDataToPropogateToCopies, context.userId());
-            LOGGER.debug("executeRequest : Updated resource ID: " + updateRes.getString(ResourceRepo.RESOURCE_ID));
-            return new ExecutionResult<>(MessageResponseFactory.createPutSuccessResponse("Location", updateRes.getString(ResourceRepo.RESOURCE_ID)),
-              ExecutionResult.ExecutionStatus.SUCCESSFUL);
-          } catch (IllegalArgumentException | SQLException e) {
-            return new ExecutionResult<>(MessageResponseFactory.createInternalErrorResponse(e.getMessage()), ExecutionResult.ExecutionStatus.FAILED);
-          }
-        }
+    if (updateRes == null) {
+      LOGGER.debug("executeRequest : We should not end up here...but if we do it is because this object is not updated in validateRequest.");
+      LOGGER.error("executeRequest : updateResource : Object to update is not found or NULL! Input resource ID: {} ", context.resourceId());
+      return new ExecutionResult<>(MessageResponseFactory.createNotFoundResponse(), ExecutionResult.ExecutionStatus.FAILED);
+    }
+
+    // try to save
+    if (!updateRes.save()) {
+      LOGGER.error("executeRequest : Update resource failed! {} ", updateRes.errors());
+      return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(updateRes.errors()), ExecutionResult.ExecutionStatus.FAILED);
+    }
+    
+    //save successful...try to propagate ownerData changes to other copies
+    if (ownerDataToPropogateToCopies != null) {
+      try {
+        DBHelper.updateOwnerDataToCopies(context.resourceId(), ownerDataToPropogateToCopies, context.userId());
+        LOGGER.debug("executeRequest : Updated resource ID: " + updateRes.getString(ResourceEntityConstants.RESOURCE_ID));
+
+      } catch (IllegalArgumentException | SQLException e) {
+        LOGGER.error("executeRequest : Update resource failed to propagate changes to other copies!", e);
+        return new ExecutionResult<>(MessageResponseFactory.createInternalErrorResponse(e.getMessage()), ExecutionResult.ExecutionStatus.FAILED);
       }
     }
-    return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(updateRes.errors()), ExecutionResult.ExecutionStatus.FAILED);
-
+    
+    return new ExecutionResult<>(MessageResponseFactory.createPutSuccessResponse("Location", updateRes.getString(ResourceEntityConstants.RESOURCE_ID)), ExecutionResult.ExecutionStatus.SUCCESSFUL);
   }
 
   @Override
