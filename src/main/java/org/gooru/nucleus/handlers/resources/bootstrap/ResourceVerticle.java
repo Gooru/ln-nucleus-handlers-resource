977a2595a8f4f6774b2b67431bf8477c05a27113
am-gooru <ashish@gooru.org>
2016-05-12 16:34:50 +0530

    Send the event instead of publishing it

--- src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/ResourceVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/ResourceVerticle.java
@@ -48,7 +48,7 @@ public class ResourceVerticle extends AbstractVerticle {
                             } else {
                                 LOGGER.warn("Invalid session token received");
                             }
-                            eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+                            eb.send(MessagebusEndpoints.MBEP_EVENT, eventData);
                         }
                     });
                 }).completionHandler(result -> {
